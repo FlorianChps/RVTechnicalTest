5f54b88631cac06fa8622ec8626c062da1da6fa6
Florian Chapuis <florian.chapuis21@gmail.com>
2021-01-06 14:30:06 +0100

    [DI] - Provide HttpClient with Hilt

--- app/src/main/java/com/fchps/rvtechnicaltest/di/ApiModule.kt
+++ app/src/main/java/com/fchps/rvtechnicaltest/di/ApiModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
@@ -21,22 +21,24 @@ object ApiModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(moshi: Moshi): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .client(
-            OkHttpClient.Builder().addInterceptor { chain ->
-                val request = chain.request()
-                val url = request.url.newBuilder()
-                    .addQueryParameter("key", "b56ac04d-d6af-4777-95a2-0ce997442d4a")
-                    .build()
-
-                chain.proceed(request.newBuilder().url(url).build())
-            }.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .build()
-        )
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .client(client).addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
+
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor { chain ->
+            val request = chain.request()
+            val url = request.url.newBuilder()
+                .addQueryParameter("key", "b56ac04d-d6af-4777-95a2-0ce997442d4a")
+                .build()
+
+            chain.proceed(request.newBuilder().url(url).build())
+        }.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
 
     @Provides
     fun provideService(retrofit: Retrofit): NavitiaService =
