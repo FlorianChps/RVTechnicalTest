59228df2f361b2aa74a33d627d436801add2e995
Florian Chapuis <florian.chapuis21@gmail.com>
2021-01-06 12:44:40 +0100

    [UI]-[FEATURE]-[STOP] - Refactor StopViewModel + remove useless annotation in StopViewHolder

--- app/src/main/java/com/fchps/rvtechnicaltest/ui/features/details/StopViewHolder.kt
+++ app/src/main/java/com/fchps/rvtechnicaltest/ui/features/details/StopViewHolder.kt
@@ -1,11 +1,9 @@
 package com.fchps.rvtechnicaltest.ui.features.details
 
 import android.graphics.Color
-import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.fchps.rvtechnicaltest.R
 
@@ -16,7 +14,6 @@ class StopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val label: TextView = itemView.findViewById(R.id.details_stop_number_commercial_mode)
     private val mode: ImageView = itemView.findViewById(R.id.details_stop_image_commercial_mode)
 
-    @RequiresApi(Build.VERSION_CODES.Q)
     fun bind(stop: StopDetailsModel) {
         departureTime.text = stop.departureHour
         mode.setImageResource(stop.type.resId)
--- app/src/main/java/com/fchps/rvtechnicaltest/ui/features/details/StopViewModel.kt
+++ app/src/main/java/com/fchps/rvtechnicaltest/ui/features/details/StopViewModel.kt
@@ -5,13 +5,10 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.fchps.rvtechnicaltest.data.entities.Departure
 import com.fchps.rvtechnicaltest.data.repository.StopRepository
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import com.fchps.rvtechnicaltest.utils.ioToMainThreadObservableTransformer
+import com.fchps.rvtechnicaltest.utils.store
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import java.text.SimpleDateFormat
-import java.util.*
 
 class StopViewModel @ViewModelInject constructor(
     private val stopRepository: StopRepository
@@ -20,48 +17,20 @@ class StopViewModel @ViewModelInject constructor(
     val stopLiveData: LiveData<List<StopDetailsModel>> get() = _stopLiveData
 
     private val _stopLiveData = MutableLiveData<List<StopDetailsModel>>()
-    private val disposable = CompositeDisposable()
+    private val transformer = ioToMainThreadObservableTransformer<List<StopDetailsModel>>()
+    private val compositeDisposable = CompositeDisposable()
 
     fun getDetails(stopId: String) {
-        disposable.add(stopRepository.getStopDetails(stopId)
-            .subscribeOn(Schedulers.io())
-            .map { result ->
-                result.departures.map { mapDepartureToStopDetailsModel(it) }
-            }
-            .observeOn(AndroidSchedulers.mainThread())
+        stopRepository.getStopDetails(stopId)
+            .compose(transformer)
             .subscribe(
                 { list -> _stopLiveData.value = list },
-                { error -> Log.e("ERROR", error.localizedMessage) }
-            ))
-    }
-
-    private fun mapDepartureToStopDetailsModel(departure: Departure): StopDetailsModel =
-        StopDetailsModel(
-            mapCommercialModeToType(departure.displayInformations.commercialMode),
-            "#${departure.displayInformations.color}",
-            departure.displayInformations.code,
-            formatDepartureHour(departure.stopDateTime.departureDateTime),
-            departure.displayInformations.direction
-        )
-
-    private fun formatDepartureHour(departureDateTime: String): String {
-        val parser = SimpleDateFormat("yyyyMMDD'T'HHmmss", Locale.FRANCE)
-        val formatter = SimpleDateFormat("HH:mm", Locale.FRANCE)
-        return formatter.format(parser.parse(departureDateTime))
-    }
-
-    private fun mapCommercialModeToType(commercialMode: String): TransportType {
-        return when (commercialMode) {
-            "Bus" -> TransportType.BUS
-            "Métro" -> TransportType.SUB
-            "RER" -> TransportType.RER
-            "Train" -> TransportType.RER
-            else -> throw IllegalArgumentException("This $commercialMode value does not match with a TransportType")
-        }
+                { error -> Log.e("ERROR", error.localizedMessage) })
+            .store(compositeDisposable)
     }
 
     override fun onCleared() {
         super.onCleared()
-        disposable.clear()
+        compositeDisposable.clear()
     }
 }
